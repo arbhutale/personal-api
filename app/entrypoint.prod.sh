4bcc4c3b4238f4fb3e127e92b37cacee019d67b2
anilbhutale <anil.bhutale@hedgehoglab.com>
2023-08-05 20:48:08 +0530

    deploy

--- app/entrypoint.prod.sh
+++ app/entrypoint.prod.sh
@@ -10,8 +10,8 @@
 
 #     echo "PostgreSQL started"
 # fi
-python manage.py flush --no-input
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
+# python manage.py flush --no-input
+# python manage.py migrate
+# python manage.py collectstatic --no-input --clear
 gunicorn  --bind 0.0.0.0:8000 hello_django.wsgi:application
 exec "$@"
